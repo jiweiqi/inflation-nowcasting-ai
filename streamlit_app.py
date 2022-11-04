4a43c7a0afda16440fded18d7bc500d856abbe94
Weiqi Ji <jiweiqi10@gmail.com>
2022-11-04 11:40:35 +0000

    update title

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,7 +5,7 @@ import pandas as pd
 import streamlit as st
 
 """
-# Project Inflation using Economic Models and Artificial Intelligence
+# Project Inflation using AI
 
 Edit `/streamlit_app.py` to customize this app to your heart's desire :heart:
 
