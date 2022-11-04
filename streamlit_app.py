2f06a566293b32d164fe4c8407b6afe0664ed3a2
Weiqi Ji <jiweiqi10@gmail.com>
2022-11-04 12:31:58 +0000

    hide code

--- streamlit_app.py
+++ streamlit_app.py
@@ -13,26 +13,27 @@ This project aims to use economic models, machine learning and statistics to pre
 """
 
 
-with st.echo(code_location='below'):
-    total_points = st.slider("Number of points in spiral", 1, 5000, 2000)
-    num_turns = st.slider("Number of turns in spiral", 1, 100, 9)
+# with st.echo(code_location='below'):
+total_points = st.slider("Number of points in spiral", 1, 5000, 2000)
+num_turns = st.slider("Number of turns in spiral", 1, 100, 9)
 
-    Point = namedtuple('Point', 'x y')
-    data = []
+Point = namedtuple('Point', 'x y')
+data = []
 
-    points_per_turn = total_points / num_turns
+points_per_turn = total_points / num_turns
 
-    for curr_point_num in range(total_points):
-        curr_turn, i = divmod(curr_point_num, points_per_turn)
-        angle = (curr_turn + 1) * 2 * math.pi * i / points_per_turn
-        radius = curr_point_num / total_points
-        x = radius * math.cos(angle)
-        y = radius * math.sin(angle)
-        data.append(Point(x, y))
+for curr_point_num in range(total_points):
+    curr_turn, i = divmod(curr_point_num, points_per_turn)
+    angle = (curr_turn + 1) * 2 * math.pi * i / points_per_turn
+    radius = curr_point_num / total_points
+    x = radius * math.cos(angle)
+    y = radius * math.sin(angle)
+    data.append(Point(x, y))
 
-    st.altair_chart(alt.Chart(pd.DataFrame(data), height=500, width=500)
-        .mark_circle(color='#0068c9', opacity=0.5)
-        .encode(x='x:Q', y='y:Q'))
+st.altair_chart(alt.Chart(pd.DataFrame(data), height=500, width=500)
+    .mark_circle(color='#0068c9', opacity=0.5)
+    .encode(x='x:Q', y='y:Q'))
+
 
 """
 disclaimer: this is a work in progress and should not be used for any financial decisions.
