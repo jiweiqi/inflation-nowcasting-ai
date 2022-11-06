9b79f7453de3d08e6ee0699d7e15ed10a0e3e2fc
Weiqi Ji <jiweiqi10@gmail.com>
2022-11-05 22:38:53 -0400

    try plotly

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,6 +3,7 @@ import altair as alt
 import math
 import pandas as pd
 import streamlit as st
+import plotly.express as px
 
 """
 # Project Inflation using AI
@@ -12,30 +13,16 @@ This project aims to use economic models, machine learning and statistics to pre
 
 """
 
-
-# with st.echo(code_location='below'):
-total_points = st.slider("Number of points in spiral", 1, 5000, 2000)
-num_turns = st.slider("Number of turns in spiral", 1, 100, 9)
-
-Point = namedtuple('Point', 'x y')
-data = []
-
-points_per_turn = total_points / num_turns
-
-for curr_point_num in range(total_points):
-    curr_turn, i = divmod(curr_point_num, points_per_turn)
-    angle = (curr_turn + 1) * 2 * math.pi * i / points_per_turn
-    radius = curr_point_num / total_points
-    x = radius * math.cos(angle)
-    y = radius * math.sin(angle)
-    data.append(Point(x, y))
-
-st.altair_chart(alt.Chart(pd.DataFrame(data), height=500, width=500)
-    .mark_circle(color='#0068c9', opacity=0.5)
-    .encode(x='x:Q', y='y:Q'))
+df = px.data.stocks()
+fig = px.line(df, x="date", y=df.columns,
+              hover_data={"date": "|%B %d, %Y"},
+              title=None)
+fig.update_xaxes(
+    dtick="M1",
+    tickformat="%b\n%Y")
+st.plotly_chart(fig, use_container_width=True)
 
 
 """
 disclaimer: this is a work in progress and should not be used for any financial decisions.
-
 """
